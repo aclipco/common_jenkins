7575d13441dd46a101b81bd8cfe5f3f6b4d5182a
Mustafa Calik <mcalikdevop@gmail.com>
2019-09-04 07:29:35 -0700

    edited

--- artemis_deployment/Jenkinsfile.groovy
+++ artemis_deployment/Jenkinsfile.groovy
@@ -13,7 +13,7 @@ node('master') {
    }
     
     stage('Generate Vars') {
-        def file = new File("${WORKSPACE}/google_grafana/grafana.tfvars")
+        def file = new File("${WORKSPACE}/google_artemis/artemis.tfvars")
         file.write """
         password              =  "${password}"
         namespace             = "${namespace}"
@@ -21,28 +21,28 @@ node('master') {
         """
       }
     stage("Terraform init") {
-      dir("${WORKSPACE}/google_grafana/") {
+      dir("${WORKSPACE}/google_artemis/") {
         sh "terraform init"
       }
     }
     stage('Terraform Apply/Plan') {
       if (params.terraformApply) {
-        dir("${WORKSPACE}/google_grafana/") {
+        dir("${WORKSPACE}/google_artemis/") {
           echo "##### Terraform Applying the Changes ####"
-          sh "terraform apply  --auto-approve  -var-file=grafana.tfvars"
+          sh "terraform apply  --auto-approve  -var-file=artemis.tfvars"
         }
     } else {
-        dir("${WORKSPACE}/google_grafana/") {
+        dir("${WORKSPACE}/google_artemis/") {
           echo "##### Terraform Plan (Check) the Changes ####"
-          sh "terraform plan -var-file=grafana.tfvars"
+          sh "terraform plan -var-file=artemis.tfvars"
       } 
     }
 }
     stage('Terraform Destoy') {
       if (params.terraformDestroy) {
-        dir("${WORKSPACE}/google_grafana/") {
+        dir("${WORKSPACE}/google_artemis/") {
           echo "##### Terraform Destroying the Changes ####"
-          sh "terraform destroy  --auto-approve  -var-file=grafana.tfvars"
+          sh "terraform destroy  --auto-approve  -var-file=artemis.tfvars"
         }
       } 
     }
